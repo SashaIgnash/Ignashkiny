67f81e480341cc4ae84f5fd97d73a193e847fdc1
is <kobe24lakers@bk.ru>
2016-01-11 14:30:12 +0300

    newstage

--- VCS/Migrations/Configuration.cs
+++ VCS/Migrations/Configuration.cs
@@ -14,6 +14,8 @@ namespace VCS.Migrations
 
         protected override void Seed(VCS.Context context)
         {
+
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
